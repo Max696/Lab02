d27b59696991f753674e4c4423070e09523295d3
Max696 <maxferdiaz@gmail.com>
2018-03-07 09:19:05 -0600

    Full working delete
    Delete have an error if root was deleted. Fix it ! : D

---
+++ WebApplication2/WebApplication2/Models/Delegados.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDA_NoLineales.Clases;
+
+namespace TDA_NoLineales.Interfaces
+{
+    public delegate int ComparadorNodosDelegate<T>(T _actual, T _nuevo);
+
+    public delegate void RecorridoDelegate<T>(Nodo<T> _acutal);
+}
